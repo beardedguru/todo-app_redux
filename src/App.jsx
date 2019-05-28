06b472348fdc10ed7faf3da97483d598fef9dd7d
beardedguru <liquidhmedia@gmail.com>
2019-05-28 17:59:49 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import ToDO from './pages/todo'
+import { Provider as ReduxProvider } from 'react-redux'
+import configureStore from './modules/store'
+
+const reduxStore = configureStore(window.REDUX_INITIAL_DATA)
+
+class App extends React.Component {
+  reunder () {
+    return (
+      <ReduxProvider store={reduxStore}>
+        <div className='App'>
+          <header className='App-header'>
+            <img src={logo} alt='logo' />
+            <h1 className='App-title'>ToDo Redux app</h1>
+          </header>
+          <ToDO />
+        </div>
+      </ReduxProvider>
+    )
+  }
+}
+
+export default App
